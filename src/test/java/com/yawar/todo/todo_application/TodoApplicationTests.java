ecc9c41d4fac33b6d4862288ff413cef6692acfd
yawar7081 <yawar7081@gmail.com>
2026-02-16 23:59:43 +0530

    Initial Setup Completed - App is Running

---
+++ src/test/java/com/yawar/todo/todo_application/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yawar.todo.todo_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
